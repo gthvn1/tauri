4bbbd68d8c808a1e98f9dbfa7d84b3a57f79b852
Guillaume <guillaume.thouvenin@vates.tech>
2026-04-16 17:38:51 +0200

    update
    - it looks like mixing unstable and stable is not working well
    - add rust dev tools
    - we use cargo tool to manage tauri

--- shell.nix
+++ shell.nix
@@ -1,25 +1,23 @@
 # Run with `nix-shell shell.nix`
 let
   pkgs = import <nixpkgs> {};
-  unstable = import <nixos-unstable> {};
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     pkg-config
     wrapGAppsHook4
     cargo
-    nodejs # require for vite
-    deno   # optional
     rustc
-  ] ++ (with unstable; [
+    clippy
+    rustfmt
+    rust-analyzer
     cargo-tauri
-  ]);
+  ];
 
   buildInputs = with pkgs; [
     librsvg
-  ] ++ (with unstable; [
     webkitgtk_4_1
-  ]);
+  ];
 
   shellHook = ''
     export XDG_DATA_DIRS="$GSETTINGS_SCHEMAS_PATH"
